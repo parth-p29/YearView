a11a0c69e790c93b2f11d5404c3ec6b39fdb47ed
Your Name <parth.spatel871@gmail.com>
2021-04-30 18:48:50 -0400

    login page template done

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, request, url_for, redirect
+from flask import Flask, request, url_for, redirect, render_template
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-
+app.config['MONGO_URI'] = 'mongodb://parthpatel:parth2911@shopifybackendchallange.dz8by.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongodb = PyMongo(app)
+app.secret_key = 'yessir'
 
 @app.route('/')
 def login():
     
-    return "we getting shopify offer for fall 2021 baby!"
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+
+    return 'hi'
